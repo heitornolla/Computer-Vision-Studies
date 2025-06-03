1f077e6fd56c1e9cf7f2baeebe832fbfb5d76026
heitor <heitor.nolla@gmail.com>
2025-06-03 11:25:45 -0300

    coded a nn, no loss yet

--- src/dense_layer.py
+++ src/dense_layer.py
@@ -5,11 +5,6 @@ from nnfs.datasets import spiral_data
 import numpy as np
 
 
-def generate_data():
-  x, y = spiral_data(100, 3)
-  return x,y
-
-
 class DenseLayer():
   def __init__(self, n_inputs, n_neurons):
     self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
@@ -17,16 +12,3 @@ class DenseLayer():
 
   def forward(self, inputs):
     self.output = np.dot(inputs, self.weights) + self.biases
-
-
-def main():
-  x, y = generate_data()
-
-  dense1 = DenseLayer(2, 3)
-  dense1.forward(x)
-
-  print(dense1.output[:5])
-
-
-if __name__ == "__main__":
-  main()
