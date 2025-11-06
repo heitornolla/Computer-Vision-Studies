5560c0d83a3543b5217a141648fee20d35652256
heitor <heitor.nolla@gmail.com>
2025-11-06 20:10:37 -0300

    new training loop

---
+++ codes/lightning/train.py
@@ -0,0 +1,26 @@
+import pytorch_lightning as pl
+from utils.model import NN
+from utils.dataset import MnistDataModule
+import utils.config as config
+
+if __name__ == "__main__":
+    model = NN(
+        input_size=config.INPUT_SIZE,
+        learning_rate=config.LEARNING_RATE,
+        num_classes=config.NUM_CLASSES,
+    )
+    dm = MnistDataModule(
+        data_dir=config.DATA_DIR,
+        batch_size=config.BATCH_SIZE,
+        num_workers=config.NUM_WORKERS,
+    )
+    trainer = pl.Trainer(
+        accelerator=config.ACCELERATOR,
+        devices=config.DEVICES,
+        min_epochs=1,
+        max_epochs=3,
+        precision=config.PRECISION,
+    )
+    trainer.fit(model, dm)
+    trainer.validate(model, dm)
+    trainer.test(model, dm)
